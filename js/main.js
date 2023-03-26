8b6250d3a405f56ea8140d58431b89e8fa93455e
devfranco <cristhianfranco@Cristhian-Franco.local>
2023-03-26 02:43:03 -0500

    se traabaja en carrousels

--- js/main.js
+++ js/main.js
@@ -2,7 +2,112 @@ $(document).ready(function() {
  
     $("#owl-demo").owlCarousel({
         items:2,
-        navigation : true
+        navigationText:["prev","next"],
+        responsive: {
+            0: {
+              items: 1,
+              nav: false,
+              center: true,
+              dots: true  
+            },
+            680: {
+              items: 1,
+              nav: false,
+              loop: true
+            },
+            1000: {
+              items: 2,
+              nav: true
+            }
+        }
     });
    
   });
+
+  $(document).ready(function() {
+ 
+    $("#tablaPremios").owlCarousel({
+        items:1,
+        responsive: {
+            0: {
+              items: 1,
+              nav: false,
+              center: true,
+              dots: true  
+            },
+            680: {
+              items: 1,
+              nav: false,
+              loop: true
+            },
+            1000: {
+              items: 1,
+              nav: true
+            }
+          }
+    });
+   
+  });
+
+  $(document).ready(function() {
+ 
+    $("#premiosAcomulativos").owlCarousel({
+        items:2,
+        responsive: {
+            0: {
+              items: 1,
+              nav: false,
+              center: true,
+              dots: true  
+            },
+            680: {
+              items: 1,
+              nav: false,
+              loop: true
+            },
+            1000: {
+              items: 1,
+              nav: true
+            }
+          }
+    });
+   
+  });
+
+
+  $(document).ready(function(){
+    let $owl = $('.owl-carousel');
+
+    $owl.children().each( function( index ) {
+      $(this).attr( 'data-position', index ); // NB: .attr() instead of .data()
+    });
+    
+    $owl.owlCarousel({
+      center: true,
+      loop: true,
+      items: 3,
+      responsive: {
+        0: {
+          items: 1,
+          nav: false,
+          center: true,
+          dots: true  
+        },
+        680: {
+          items: 2,
+          nav: false,
+          loop: true
+        },
+        1000: {
+          items: 3,
+          nav: true
+        }
+      }
+    });
+    
+    
+    $(document).on('click', '.owl-item>div', function() {
+      let $speed = 300;  // in ms
+      $owl.trigger('to.owl.carousel', [$(this).data( 'position' ), $speed] );
+    });
+  })
